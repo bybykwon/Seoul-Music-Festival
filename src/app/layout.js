398bb435f42ff62e269294a5727c2584f4d5be9f
bybykwon <byoungyoonkwon@google.com>
2025-02-14 20:08:15 +0900

    meta 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,35 +7,62 @@ import SkipNav from '@/components/layout/header/SkipNav';
 import Head from 'next/head';
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang='ko'>
-            <Head>
-                <title>Seoul-Music-Festival</title>
-                <meta
-                    name='description'
-                    content='권병윤 개인프로젝트'
-                />
-                <meta
-                    property='og:image'
-                    content='/images/titleImage.jpg'
-                />
-                <meta
-                    property='og:url'
-                    content='http://www.mysite.com'
-                />
-                {/* 기타 메타 태그 */}
-            </Head>
-            <body>
-                <Providers>
-                    <SkipNav />
-                    <div
-                        id='wrap'
-                        className='min-h-screen flex flex-col'
-                    >
-                        {children}
-                    </div>
-                </Providers>
-            </body>
-        </html>
-    );
+  return (
+    <html lang='ko'>
+      <Head>
+        <title>Seoul-Music-Festival</title>
+        <meta
+          property='og:type'
+          content='권병윤 개인프로젝트'
+        />
+        <meta
+          property='og:title'
+          content='Seoul-Music-Festival'
+        />
+        <meta
+          property='og:description'
+          content='권병윤 개인프로젝트'
+        />
+        <meta
+          property='og:image'
+          content='/images/titleImage.jpg'
+        />
+        <meta
+          property='og:url'
+          content='http://www.mysite.com/article/article1.html'
+        />
+        <meta
+          name='twitter:card'
+          content='summary'
+        />
+        <meta
+          name='twitter:title'
+          content='Seoul-Music-Festival'
+        />
+        <meta
+          name='twitter:description'
+          content='권병윤 개인프로젝트'
+        />
+        <meta
+          name='twitter:image'
+          content='http://www.mysite.com/article/article1.html'
+        />
+        <meta
+          name='twitter:domain'
+          content='Seoul-Music-Festival'
+        />
+      </Head>
+      <body>
+        <Providers>
+          <SkipNav />
+          <div
+            id='wrap'
+            className='min-h-screen flex flex-col'
+          >
+            {children}
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
